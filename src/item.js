ab50ae80f127df4e312d0a61d0074c8ba8ac0eca
Joseph Kessler <kesslerjx@icloud.com>
2022-04-28 09:22:17 -0400

    Create item object file

---
+++ src/item.js
@@ -0,0 +1,9 @@
+
+export default class Item {
+    constructor(description, note='', url='', date='') {
+        this.description = description;
+        this.note = note;
+        this.url = url;
+        this.date = date;
+    }
+}
